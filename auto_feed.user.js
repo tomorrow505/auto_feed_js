4e6fb95cd0ae65c7e484e59a03e0464b928df019
tomorrow505 <32202634+tomorrow505@users.noreply.github.com>
2022-11-18 21:56:24 +0800

    Update auto_feed.user.js

--- auto_feed.user.js
+++ auto_feed.user.js
@@ -80,12 +80,11 @@
 // @resource     css http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css
 // @require      https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js
 // @require      https://unpkg.com/tippy.js@6/dist/tippy-bundle.umd.js
-// @require      https://unpkg.com/auto-feed-media@1.0.1/index.js
 // @require      https://greasyfork.org/scripts/430180-imgcheckbox2/code/imgCheckbox2.js?version=956211
 // @require      https://greasyfork.org/scripts/444988-music-helper/code/music-helper.js?version=1079125
 // @icon         https://kp.m-team.cc//favicon.ico
 // @run-at       document-end
-// @version      1.9.9.9
+// @version      2.0.0.0
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setClipboard
 // @grant        GM_setValue
@@ -137,7 +136,7 @@
     20220708：适配支持TVV转出。
     20220710：优化皮转入，修复GPW部分bug，简单支持一键签到(测试中)。
     20220716：继续完善一键签到。修复部分bug。
-    20220726：支持MP4/MKV视频文件获取mediainfo+截图。功能待测试
+    20220726：支持MP4/MKV视频文件获取mediainfo+截图。功能待测试(已取消)...https://unpkg.com/auto-feed-media@1.0.1/index.js
     20220730：适配MV类型转发，修复部分bug。
     20220802：支持转入ITZMX、NapQAQ、HDPt(明教)，修复1PTBA、3Wmg部分bug。(by shmt86)
     20220807：一键签到取消天空和北洋，增加支持妞的转入。具体见教程：https://github.com/tomorrow505/auto_feed_js/wiki/%E8%BD%AC%E8%BD%BD%E5%88%B0BTN
@@ -149,6 +148,7 @@
     20221013：适配monika，修复部分bug。
     20221021：适配织梦PT，修复部分bug。
     20221103：适配红叶PT。
+    20221117：修复部分bug，去掉mediainfo和截图功能，鸡肋而且占用体积大。修复没有猫不能进入设置页面的bug。第一次NP站点应该都可以设置。
 */
 
 //获取网页地址，有很多种可能，首先是简单处理页面，及时返回，另外一种匹配上发布页面，一种匹配上源页面，分别处理两种逻辑
@@ -793,8 +793,8 @@ var imdb_chosen = GM_getValue('imdb_chosen') === undefined ? 0: GM_getValue('imd
 * 设置依托站点，可以选择【PTer/LemonHD/HDDolby/CMCT/PThome/OurBits/TCCF/TJUPT】
 * 实在打不开可以打开以上站点个人设置页面加#setting, 如：https://springsunday.net/usercp.php?action=personal#setting
 */
-const default_setting_host = 'PTer';
-var setting_host = GM_getValue('setting_host') === undefined ? default_setting_host: GM_getValue('setting_host');
+// const default_setting_host = 'PTer';
+var setting_host = GM_getValue('setting_host') === undefined ? null: GM_getValue('setting_host');
 
 //用来转存海报使用的ptpimg的key,打开首页即可获取
 var used_ptp_img_key = GM_getValue('used_ptp_img_key') === undefined ? '': GM_getValue('used_ptp_img_key');
@@ -1033,6 +1033,7 @@ const default_search_list = [
     `<a href="https://search.douban.com/movie/subject_search?search_text={imdbid}" target="_blank">Douban</a>`,
     `<a href="https://uhdbits.org/torrents.php?searchstr={imdbid}" target="_blank">UHD</a>`,
     `<a href="http://zmk.pw/search?q={search_name}" target="_blank">ZMK</a>`,
+    `<a href="https://mediaarea.net/MediaInfoOnline" target="_blank">MediaiInfo</a>`,
     `<a href="https://assrt.net/sub/?searchword={search_name}" target="_blank">SSW</a>`,
 ];
 
@@ -3212,6 +3213,8 @@ function getDoc(url, meta, callback) {
             if (responseDetail.status === 200) {
                 let doc = page_parser(responseDetail.responseText);
                 callback(doc, responseDetail, meta);
+            } else {
+                callback('error', null, null);
             }
         }
     });
@@ -6289,192 +6292,6 @@ if (site_url.match(/^https:\/\/.*?usercp.php\?action=personal(#setting|#ptgen|#m
     $('#mediainfo').append(`<input type="button" id="simplify" value="简化信息" style="margin-bottom:5px"><br>`);
     $('#mediainfo').append(`<textarea id="clarify_media_info" style="width:700px" rows="20"></textarea><br>`);
 
-    $('#mediainfo').append(`<div id="video-demo-container">
-        <button id="upload-button" style="width:320px; margin-left:160px">Select MP4/MKV Video</button>
-        <input type="file" id="file-to-upload" accept="video/mp4,video/x-matroska" />
-        <video id="main-video" controls>
-            <source type="video/mp4">
-        </video>
-        <div id="video-canvas">
-        </div>
-        <textarea id="mediainfo_output" style="width:640px; margin-bottom:5px" rows="30"></textarea>
-        <div id="thumbnail-container">
-            <span>截图展示：<a class="upload_img" href="https://imgbox.com" style="color:blue">-->上传imgbox<--</a> | 
-            <a class="upload_img" href="https://www.imagebam.com/" style="color:blue">-->上传imagebam<--</a> 
-            </span><br>
-        </div>
-    </div>`);
-
-    var new_style = `
-        #video-demo-container {
-            width: 800px;
-            margin: 40px auto;
-        }
-
-        #main-video {
-            display: none;
-            max-width: 400px;
-        }
-
-        #thumbnail-container {
-            display: none;
-        }
-
-        #video-canvas {
-            display: none;
-        }
-
-        #upload-button {
-            width: 150px;
-            display: block;
-            margin: 20px auto;
-        }
-
-        #file-to-upload {
-            display: none;
-        }
-    `;
-    GM_addStyle(new_style);
-
-    for (var i = 0; i < 6; i++) {
-        $('#video-canvas').append(`<canvas class="canvas"></canvas>`);
-        $('#thumbnail-container').append(`<a href=""><img style="max-width:320px; display:inline-block;margin-right:6px;" src="" /></a>`)
-    }
-
-    var _CANVAS = document.querySelectorAll(".canvas");
-    var _CTXS = [];
-    for (var i = 0; i < 6; i++) {
-        var _CTX = _CANVAS[i].getContext("2d");
-        _CTXS.push(_CTX);
-    }
-    _VIDEO = document.querySelector("#main-video");
-
-    document.querySelector("#upload-button").addEventListener('click', function() {
-        document.querySelector("#file-to-upload").click();
-    });
-
-    document.querySelector("#file-to-upload").addEventListener('change', function() {
-        // Validate whether MP4/MKV
-        file = document.querySelector("#file-to-upload").files[0];
-        console.log(file.type)
-        if(['video/mp4', 'video/x-matroska'].indexOf(file.type) == -1) {
-            alert('Error : Only MP4/MKV format allowed');
-            return;
-        }
-        var video_name = file.name;
-        $('.upload_img').click((e)=>{
-            var imgs = [];
-            e.preventDefault();
-            $(`#thumbnail-container`).find(`img`).map((index,el)=>{
-                imgs.push($(el).attr('src'));
-            });
-            imgs.push(video_name);
-            GM_setValue("64imgs", JSON.stringify(imgs));
-            window.open(e.target.href, '_blank');
-        });
-        // Object Url as the video source
-        document.querySelector("#main-video source").setAttribute('src', URL.createObjectURL(document.querySelector("#file-to-upload").files[0]));
-
-        // Load the video and show it
-        _VIDEO.load();
-        // _VIDEO.style.display = 'inline';
-
-        // Load metadata of the video to get video duration and dimensions
-        _VIDEO.addEventListener('loadedmetadata', function() {
-            var video_duration = _VIDEO.duration,
-            duration_options_html = '';
-
-            var step = Math.floor(video_duration/7);
-            var times = [];
-            for (var i = 0; i < 6; i++) {
-                times.push((i+1)*step);
-            }
-
-            Array.from(_CANVAS).forEach((item)=>{
-                item.width = _VIDEO.videoWidth;
-                item.height = _VIDEO.videoHeight;
-            });
-
-            times.forEach((t,index)=>{
-                setTimeout(function(){
-                    _VIDEO.currentTime = t;
-                }, index*3000);
-
-            });
-            $(`#thumbnail-container`).css('display', 'block');
-        });
-
-        var img_index = 0;
-        _VIDEO.addEventListener('seeked', () => {
-                _CTXS[img_index].drawImage(_VIDEO, 0, 0, _VIDEO.videoWidth, _VIDEO.videoHeight);
-                $(`#thumbnail-container`).find(`img:eq(${img_index})`).attr('src', _CANVAS[img_index].toDataURL());
-                $(`#thumbnail-container>a:eq(${img_index})`).attr('href', _CANVAS[img_index].toDataURL());
-                $(`#thumbnail-container>a:eq(${img_index})`).attr('download', `${video_name}-thumbnail-${img_index}.png`);
-                img_index += 1;
-        });
-    });
-
-    function add_drop_function(el) {
-        el.addEventListener("dragenter",function(e){  //拖进
-            e.preventDefault();
-        })
-        el.addEventListener("dragover",function(e){  //拖来拖去 
-            e.preventDefault();
-        })
-        el.addEventListener('drop', function(event) {
-            event.preventDefault();
-            document.querySelector("#file-to-upload").files = event.dataTransfer.files;
-            document.querySelector("#file-to-upload").dispatchEvent(evt);
-        });
-    }
-    add_drop_function(document.querySelector("#upload-button"));
-    add_drop_function(document.querySelector("#mediainfo_output"));
-
-    try {
-        const fileinput = document.getElementById('file-to-upload');
-        const output = document.getElementById('mediainfo_output');
-
-        const onChangeFile = (mediainfo) => {
-          const file = fileinput.files[0];
-          if (file) {
-            output.value = 'Working…'
-            const getSize = () => file.size
-            const readChunk = (chunkSize, offset) =>
-              new Promise((resolve, reject) => {
-                const reader = new FileReader()
-                reader.onload = (event) => {
-                  if (event.target.error) {
-                    reject(event.target.error)
-                  }
-                  resolve(new Uint8Array(event.target.result))
-                }
-                reader.readAsArrayBuffer(file.slice(offset, offset + chunkSize))
-              })
-
-            mediainfo
-                .analyzeData(getSize, readChunk)
-                .then((result) => {
-                    result = result.replace(/Movie name.*\n/, '');
-                    if (result.match(/(Unique ID.*\n)/)) {
-                        result = result.replace(/(Unique ID.*\n)/, `$1Complete name                            : ${file.name}\n`);
-                    } else {
-                        result = result.replace(/(General.*\n)/, `$1Complete name                            : ${file.name}\n`);
-                    }
-                    output.value = result;
-                })
-                .catch((error) => {
-                  output.value = `An error occured:\n${error.stack}`
-                })
-          }
-        }
-
-        MediaInfo({ format: 'text' }, (mediainfo) => {
-            fileinput.addEventListener('change', () => onChangeFile(mediainfo))
-        });
-    } catch(err) {
-        $('#mediainfo_output').css('display', 'none');
-    }
-
     $('#simplify').click(function(){
         var mediainfo_text = simplifyMI($('#media_info').val(), null);
         $('#clarify_media_info').val(mediainfo_text);
@@ -6486,10 +6303,11 @@ if (site_url.match(/^https:\/\/.*?usercp.php\?action=personal(#setting|#ptgen|#m
     $('#dealimg').append(`<input type="button" id="send_ptpimg" value="转ptpimg" style="margin-bottom:5px;margin-left:5px">`);
     $('#dealimg').append(`<input type="button" id="send_pixhost" value="转pixhost" style="margin-bottom:5px;margin-left:5px">`);
     $('#dealimg').append(`<input type="button" id="send_imgbox" value="转imgbox" style="margin-bottom:5px;margin-left:5px">`);
+    $('#dealimg').append(`<input type="button" id="send_hdbits" value="转HDBits" style="margin-bottom:5px;margin-left:5px">`);
     $('#dealimg').append(`<input type="button" id="get_imgbb" value="imgbb源图" style="margin-bottom:5px;margin-left:5px">`);
-    $('#dealimg').append(`<input type="button" id="350px" value="350px缩略" style="margin-bottom:5px;margin-left:5px">`);
     $('#dealimg').append(`<input type="button" id="change" value="字符串替换" style="margin-bottom:5px;margin-left:5px">`);
     $('#dealimg').append(`<input type="text" style="width: 50px; text-align:center; margin-left: 5px" id="img_source" />--<input type="text" style="width: 50px; text-align:center; margin-right: 5px" id="img_dest" /><br>`);
+    $('#dealimg').append(`<input type="button" id="350px" value="350px缩略" style="margin-bottom:5px;margin-right:5px">`);
     $('#dealimg').append(`<input type="button" id="enter2space" value="换行->空格" style="margin-bottom:5px;margin-right:5px">`);
     $('#dealimg').append(`<input type="button" id="get_encode" value="图片提取" style="margin-bottom:5px;margin-right:5px">`);
     $('#dealimg').append(`从第<input type="text" style="width: 30px; text-align:center; margin-left: 5px; margin-right:5px" id="start" />张开始每隔<input type="text" style="width: 30px; text-align:center; margin-left: 5px; margin-right:5px" id="step" />张获取其中第<input type="text" style="width: 30px; text-align:center; margin-left: 5px;margin-right:5px" id="number" />张。<br>`);
@@ -6532,14 +6350,14 @@ if (site_url.match(/^https:\/\/.*?usercp.php\?action=personal(#setting|#ptgen|#m
             } else if (item.match(/pixhost/)) {
                 pic_info += '[img]' + item.replace('//t', '//img').replace('thumbs', 'images') + '[/img]\n';
             } else if (item.match(/pterclub.com|beyondhd.co\/images/)) {
-                pic_info += '[img]' + item.replace(/th.png/, 'png') + '[/img]\n';
+                pic_info += '[img]' + item.replace(/th.png/, 'png').replace(/md.png/, 'png') + '[/img]\n';
             } else if (item.match(/tu.totheglory.im/)) {
                 pic_info += '[img]' + item.replace(/_thumb.png/, '.png') + '[/img]\n';
             } else if (item.match(/img.hdchina.org/)) {
                 pic_info += '[img]' + item.replace(/md.png/, 'png') + '[/img]\n';
             } else if (item.match(/cinematik/)) {
                 pic_info += '[img]' + item.replace(/thu/, 'big') + '[/img]\n';
-            } else if (item.match(/img4k.net/)) {
+            } else if (item.match(/img4k.net|img.hdhome.org/)) {
                 pic_info += '[img]' + item.replace(/md.png/, 'png') + '[/img]\n';
             } else {
                 pic_info += '[img]' + item + '[/img]\n';
@@ -6587,6 +6405,24 @@ if (site_url.match(/^https:\/\/.*?usercp.php\?action=personal(#setting|#ptgen|#m
             images.push(name);
             GM_setValue('HDB_images', images.join(', '));
             window.open('https://imgbox.com/', '_blank');
+        }
+    });
+
+    $('#send_hdbits').click((e)=>{
+        var origin_str = $('#picture').val();
+        images = origin_str.match(/\[img\]http[^\[\]]*?(jpg|png|webp)/g).map((item)=>{ return item.replace(/\[.*?\]/g, ''); });
+        if (images.length) {
+            var name = 'set your gallary name';
+            try {
+                if (descr.match(/Disc Title:/)) {
+                    name = descr.match(/Disc Title:(.*)/)[1].trim();
+                } else if (descr.match(/Complete name.*?:/i)) {
+                    name = descr.match(/Complete name.*?:(.*)/)[1].trim();
+                }
+            } catch(err) {console.log(err)}
+            images.push(name);
+            GM_setValue('HDB_images', images.join(', '));
+            window.open('https://img.hdbits.org/', '_blank');
         }
     });
 
@@ -6660,10 +6496,20 @@ if (site_url.match(/^https:\/\/.*?usercp.php\?action=personal(#setting|#ptgen|#m
             })
             return p;
         }
+        function getpostdoc(url) {
+            var p = new Promise((resolve, reject)=>{
+                getDoc(url,null,function(doc){
+                    console.log(doc)
+                    var source_img_url = $('#download', doc).attr('href').split('?')[0];
+                    resolve(source_img_url);
+                });
+            })
+            return p;
+        }
         var origin_str = $('#picture').val();
         var imgbb_urls = origin_str.match(/https?:\/\/i.ibb.co[^\[\]]*?(jpg|png)/g);
-        if (!imgbb_urls.length) {
-            alert("没有监测到imgbb链接");
+        if (imgbb_urls === null) {
+            // alert("没有监测到imgbb链接");
         } else {
             var imgbb_tasks = [];
             imgbb_urls.map(item=>{
@@ -6674,6 +6520,26 @@ if (site_url.match(/^https:\/\/.*?usercp.php\?action=personal(#setting|#ptgen|#m
             Promise.all(imgbb_tasks).then((data)=>{
                 for (i=0; i<data.length; i++) {
                     origin_str = origin_str.replace(imgbb_urls[i], data[i]);
+                }
+                origin_str = origin_str.match(/\[img\]https?:.*?(jpg|png)\[\/img\]/g).join('\n');
+                $('#result').val(origin_str);
+            })
+        }
+        var postimg_urls = origin_str.match(/https?:\/\/i.postimg.cc[^\[\]]*?(jpg|png)/g);
+        if (postimg_urls === null) {
+            // alert("没有监测到imgbb链接");
+        } else {
+            var imgpost_tasks = [];
+            postimg_urls.map(item=>{
+                var imgpost_show_url = 'https://postimg.cc/' + item.match(/https:\/\/i.postimg.cc\/(.*?)\//)[1];
+                console.log(imgpost_show_url)
+                var imgpost_p = getpostdoc(imgpost_show_url);
+                imgpost_tasks.push(imgpost_p);
+            })
+            Promise.all(imgpost_tasks).then((data)=>{
+                console.log(data)
+                for (i=0; i<data.length; i++) {
+                    origin_str = origin_str.replace(postimg_urls[i], data[i]);
                 }
                 origin_str = origin_str.match(/\[img\]https?:.*?(jpg|png)\[\/img\]/g).join('\n');
                 $('#result').val(origin_str);
@@ -12209,6 +12075,22 @@ setTimeout(function(){
                             window.open(site_href, '_blank');
                         }
                     }
+                }
+            });
+        }
+
+        if (setting_host == null) {
+            var url = default_site_info[origin_site].url + 'usercp.php?action=personal';
+            getDoc(url, null, function(doc){
+                if ($('#usercpnav', doc).length) {
+                    $(forward_r).find('a').map((index,e)=>{
+                        var jump_url = $(e).attr('href');
+                        if (jump_url.match(/^undefined/)) {
+                            $(e).attr('href', jump_url.replace('undefined', default_site_info[origin_site].url + 'usercp.php?action=personal'))
+                        }
+                    });
+                } else {
+                    alert('选择一个NP架构站点进行脚本托管站点设置！！');
                 }
             });
         }
