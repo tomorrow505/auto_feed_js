3b21cec6ea3f8d4c7976f53e5691637657fe1274
tomorrow505 <32202634+tomorrow505@users.noreply.github.com>
2024-11-11 22:10:57 +0800

    Update auto_feed.user.js

--- auto_feed.user.js
+++ auto_feed.user.js
@@ -8634,7 +8634,7 @@ function auto_feed() {
                     raw_info.torrent_url = used_site_info[origin_site].url + $('a[href*="download.php"]:contains(下载地址)').attr('href');
                 } else if ($('td:contains(种子链接)').length) {
                     raw_info.torrent_url = used_site_info[origin_site].url + $('td:contains(种子链接)').next().find('a').attr('href');
-                    if (raw_info.torrent_url.includes('http')) {
+                    if ($('td:contains(种子链接)').next().find('a').attr('href').includes('http')) {
                         raw_info.torrent_url = $('td:contains(种子链接)').next().find('a').attr('href');
                     }
                 } else if ($('td:contains(下载直链)').length) {
