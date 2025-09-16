673086a8d840b37dc23ddef8b4effd0788c7d5c0
mojieafei <daveafei@hotmail.com>
2025-09-16 09:58:46 +0800

    增加天枢dubhe.site支持

--- auto_feed.user.js
+++ auto_feed.user.js
@@ -1063,6 +1063,7 @@ const default_site_info = {
     'KuFei': {'url': 'https://kufei.org/', 'enable': 1},
     'RouSi': {'url': 'https://rousi.zip/', 'enable': 1},
     '悟空': {'url': 'https://wukongwendao.top/', 'enable': 1},
+    '天枢': {'url': 'https://dubhe.site/', 'enable': 1},
     'GTK': {'url': 'https://pt.gtk.pw/', 'enable': 1},
     '象岛': {'url': 'https://ptvicomo.net/', 'enable': 1},
     '麒麟': {'url': 'https://www.hdkyl.in/', 'enable': 1},
@@ -21047,6 +21048,64 @@ function auto_feed() {
             }
             $('select[name="team_sel[4]"]').val(5);
             check_team(raw_info, 'team_sel[4]');
+        }
+
+        else if (forward_site == '天枢') {
+            var browsecat = $('#browsecat');
+            var type_dict = {
+                '电影': 401, '剧集': 402, '综艺': 403, '音乐': 408, '纪录': 404,
+                '动漫': 405, '体育': 407, 'MV': 406, '软件': 409, '学习': 409, '': 409
+            };
+            browsecat.val(409);
+            if (type_dict.hasOwnProperty(raw_info.type)) {
+                var index = type_dict[raw_info.type];
+                browsecat.val(index);
+            }
+            try { disableother('browsecat','specialcat'); } catch (err) {}
+
+            // 分辨率映射: 1080p=1, 1080i=2, 720p=3, SD=4, 2160p=5, 2160i=6
+            var standard_box = $('select[name="standard_sel"]');
+            var standard_dict = {
+                '1080p': 1, '1080i': 2, '720p': 3, 'SD': 4, '4K': 5, '2160p': 5, '2160i': 6
+            };
+            if (standard_dict.hasOwnProperty(raw_info.standard_sel)) {
+                var index = standard_dict[raw_info.standard_sel];
+                standard_box.val(index);
+            }
+
+            // 标准的NexusPHP字段处理
+            var medium_box = $('select[name="medium_sel"]');
+            switch(raw_info.medium_sel) {
+                case 'UHD': medium_box.val(1); break;
+                case 'Blu-ray': medium_box.val(2); break;
+                case 'DVD': medium_box.val(3); break;
+                case 'Remux': medium_box.val(4); break;
+                case 'HDTV': medium_box.val(5); break;
+                case 'Encode': medium_box.val(6); break;
+                case 'WEB-DL': medium_box.val(7); break;
+                case 'CD': medium_box.val(8);
+            }
+
+            var codec_box = $('select[name="codec_sel"]');
+            switch (raw_info.codec_sel) {
+                case 'H264': case 'X264': codec_box.val(1); break;
+                case 'H265': case 'X265': codec_box.val(2); break;
+                case 'VC-1': codec_box.val(3); break;
+                case 'MPEG-2': case 'MPEG-4': codec_box.val(4); break;
+                case 'XVID': codec_box.val(5);
+            }
+
+            var audiocodec_box = $('select[name="audiocodec_sel"]');
+            switch (raw_info.audiocodec_sel) {
+                case 'DTS-HD': case 'DTS-HDMA': case 'DTS-HDHR': audiocodec_box.val(1); break;
+                case 'TrueHD': case 'Atmos': audiocodec_box.val(2); break;
+                case 'DTS': audiocodec_box.val(3); break;
+                case 'AC3': audiocodec_box.val(4); break;
+                case 'AAC': audiocodec_box.val(5); break;
+                case 'Flac': audiocodec_box.val(6); break;
+                case 'APE': audiocodec_box.val(7); break;
+                case 'WAV': audiocodec_box.val(8);
+            }
         }
 
         else if (forward_site == 'CarPt') {
